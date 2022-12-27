5b19797d9c4c59ad098674efe4321ce679abe926
jeremias <blackarrowsb@gmail.com>
2022-12-27 10:38:00 -0300

    formulario login

--- js/app.js
+++ js/app.js
@@ -10,6 +10,22 @@ function cambiarModoColor(button){
   button.classList.toggle("darkmode");
 }
 
+document.querySelector("#show-login").addEventListener("click",function(){
+  document.querySelector(".popup").classList.remove("active");
+});
+
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+  document.querySelector(".popup").classList.add("active");
+});
+
+/* loggin crea usuario y lo guardamos en el Localstorage */
+  
+// let usuario=prompt("ingrese un su nombre");
+// const passUser=prompt("ingrese su password");
+
+// localStorage.setItem("usuario", usuario)
+
+// let usuarioLocalstorage = localStorage.getItem("usuario");
 
 
 
