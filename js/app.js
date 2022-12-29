01006b99b4dd2ba8a25ab477d617ff4250a01165
jeremias <blackarrowsb@gmail.com>
2022-12-29 08:34:59 -0300

    cargamos datos al localStorage

--- js/app.js
+++ js/app.js
@@ -43,32 +43,73 @@ datosFormularioLogin.reset();
 
 
 const datosFormularioLogin = document.querySelector("#loginForm");
-const datosFormularioSignUp = document.querySelector("#signUpnForm");
+const datosFormularioSignUp = document.querySelector("#signUpForm");
 let signUpName = document.querySelector("#signUpName");
 let signUpUserName = document.querySelector("#signUpUserName");
 let signUpPassword = document.querySelector("#signUpPassword");
 let ConfirmPassword = document.querySelector("#ConfirmPassword");
-let loginUser = document.querySelector("#loginUser");
-let loginPass = document.querySelector("#loginPassword");
+//login
 
-signUpName.addEventListener("input", function () {
-  console.log(signUpName.value);
-})
-signUpUserName.addEventListener("input", function () {
-  console.log(signUpUserName.value);
-})
-signUpPassword.addEventListener("input", function () {
-  console.log(signUpPassword.value);
-})
-ConfirmPassword.addEventListener("input", function () {
-  console.log(ConfirmPassword.value);
-})
-loginUser.addEventListener("input", function () {
-  console.log(loginUser);
-})
-loginPass.addEventListener("input", function () {
-  console.log(loginPass.value);
-})
+//lectura de datos del login y guardado en objeto
+let baseDatos = [];
+document.querySelector("#signIn").addEventListener("click",function(e){
+  e.preventDefault(); // no me borra todos los datos del array
+let loginUser = document.querySelector("#loginUser").value; //capturo los datos
+let loginPass = document.querySelector("#loginPassword").value;
+
+//clase constructora
+class Users {
+  constructor(loginUser, loginPass){
+    this.user = loginUser;
+    this.pass = loginPass;
+  }
+}
+persona = new Users(loginUser, loginPass)
+console.log(persona);
+aagregar();
+});
+
+  function aagregar(){
+    baseDatos.push(persona);
+    console.log(baseDatos);
+    localStorage.setItem("usuarios", JSON.stringify(baseDatos));
+  }
+
+// let userlogin = [];
+// userlogin.push(persona);
+// localStorage.setItem("Usuarios", JSON.stringify(userlogin));
+
+// function User(loginUser, loginPass){
+//   this.loginUser = loginUser;
+//   this.loginPass = loginPass;
+// }
+// const NUser = new User (loginUser,loginPass);
+// console.log(NUser);
+
+
+
+
+//sign up
+// signUpName.addEventListener("input", function () {
+//   console.log(signUpName.value);
+// })
+// signUpUserName.addEventListener("input", function () {
+//   console.log(signUpUserName.value);
+// })
+// signUpPassword.addEventListener("input", function () {
+//   console.log(signUpPassword.value);
+// })
+// ConfirmPassword.addEventListener("input", function () {
+//   console.log(ConfirmPassword.value);
+// })
+
+//login
+// loginUser.addEventListener("input", function () {
+//   console.log(loginUser);
+// })
+// loginPass.addEventListener("input", function () {
+//   console.log(loginPass.value);
+// })
 
 
 
