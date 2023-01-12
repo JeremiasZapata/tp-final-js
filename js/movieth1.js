d15066e07af616e37fa0cfa0a09fde2bef545edf
jeremias <blackarrowsb@gmail.com>
2023-01-12 20:59:50 -0300

    ultimos cambios de hoy bis

--- js/movieth1.js
+++ js/movieth1.js
@@ -25,6 +25,8 @@ function cambiarModoColor(button) {
     button.classList.toggle("darkmode");
 }
 
+
+
 // Recuperamos el valor de backGrooundOnLs que enviamos desde index con el id
 
 let classBackGround = localStorage.getItem("backGroundOnLs");
@@ -119,42 +121,3 @@ btnreservar.addEventListener("click", function () {
 
 
 
-
-
-
-
-
-// function formularioDias() {
-//     classBackGround.forEach(x => {
-//         const diasHorarios = document.createElement('option');
-//         diasHorarios.innerHTML = `
-//         ${x.dia}
-//         `
-//         dayCreator.appendChild(diasHorarios)
-//     })
-//     console.log(classBackGround)
-// }
-
-// formularioDias();
-
-
-
-
-
-// seleccion de dia
-
-////--------OPCIONES MULTIPLES--------------//////
-
-// let optionSelected = document.querySelectorAll(".form-select");
-// let btnreservar = document.querySelector(".btn-reservar");
-
-// btnreservar.addEventListener("click", function () {
-//     let dia = document.querySelector("#day").value;
-//     let hora = document.querySelector("#hr").value;
-
-//     let selector = document.querySelector(".selector");
-//     selector.innerHTML = `
-// <div class="alert alert-success" role="alert">
-// <h5>Vas a asistir el dia ${dia} a las ${hora} hs.</h5>
-// </div>`;
-// });
