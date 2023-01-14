cb83325ccb0f888e4324b72a437635ff0d247c45
jeremias <blackarrowsb@gmail.com>
2023-01-14 15:57:53 -0300

    terminadas las cards con fetch

--- js/cards.js
+++ js/cards.js
@@ -1,12 +1,11 @@
 //******************************************************************************** */
 //card premmier con json
 
-fetch("../js/movies.json")
+fetch("../js/premmier.json")
     .then(response => response.json())
-    
     .then (premmier => {
 
-        const premiere = data["premmier"]
+    const moviesPremiere = premmier;
 
         function movieCardPremmiere(){ 
     moviesPremiere.forEach(el => { 
@@ -41,76 +40,125 @@ movieCardPremmiere();
 
 //************************************************************************************************ */
 
+fetch("../js/recommended.json")
+    .then(response => response.json())
+    .then (recommended => {
+
+    const moviesRecommended = recommended;
+
+    function movieCardRecommended(){
+        moviesRecommended.forEach(el => {
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="card ${el.img}" id="card-1">
+                    <div class="content">
+                        <h2>${el.sala}</h2>
+                        <h3>Movie theatre</h3>
+                        <p>${el.descripcion}</p>
+                        <a href="./pages/movieTh1.html" id="${el.nommbre}">Buy Ticket</a>
+                    </div>
+            </div>
+            `
+            cardRecommendedCreator.appendChild(div)
+            const backGroundToLs = document.getElementById(`${el.nommbre}`)
+            backGroundToLs.addEventListener("click", () => {
+                console.log(`${el.nommbre}`)
+                localStorage.setItem("backGroundOnLs", `${el.nommbre}`);
+            })
+            VanillaTilt.init(document.querySelectorAll(".card"), {
+                max: 4,
+                speed: 400
+                });
+        })
+    }
+    
+    movieCardRecommended();
+})
 
 
-const moviesRecommended = [
-    {
-        nommbre: "backToTheFuture",
-        duracion: 120,
-        rate: "ATP",
-        descripcion: "Es una re pelicula re copada con muchos efectos",
-        sala: "04",
-        movieId: 001,
-        img:"imgTen", 
-    },
-    {
-        nommbre: "indianaJones",
-        duracion: 120,
-        rate: "ATP",
-        descripcion: "Es una re pelicula re copada con muchos efectos",
-        sala: "05",
-        movieId: 001,
-        img:"imgEleven", 
-    },
-    {
-        nommbre: "starWars",
-        duracion: 120,
-        rate: "ATP",
-        descripcion: "Es una re pelicula re copada con muchos efectos",
-        sala: "06",
-        movieId: 001,
-        img:"imgTwelve", 
-    },
-]
+//********************************************************************************************************** */
 
-const moviesCassic = [
-    {
-        nommbre: "pulpFiction",
-        duracion: 120,
-        rate: "ATP",
-        descripcion: "Es una re pelicula re copada con muchos efectos",
-        sala: "04",
-        movieId: 001,
-        img:"imgThirteen", 
-    },
-    {
-        nommbre: "taxiDriver",
-        duracion: 120,
-        rate: "ATP",
-        descripcion: "Es una re pelicula re copada con muchos efectos",
-        sala: "05",
-        movieId: 001,
-        img:"imgFourteen", 
-    },
-    {
-        nommbre: "godFather",
-        duracion: 120,
-        rate: "ATP",
-        descripcion: "Es una re pelicula re copada con muchos efectos",
-        sala: "06",
-        movieId: 001,
-        img:"imgFifteen", 
-    },
-]
+fetch("../js/classic.json")
+    .then(response => response.json())
+    .then (classic => {
+
+    const moviesClassic = classic;
+
+    function movieCardClassic(){
+        moviesClassic.forEach(el => {
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="card ${el.img}" id="card-1">
+                    <div class="content">
+                        <h2>${el.sala}</h2>
+                        <h3>Movie theatre</h3>
+                        <p>${el.descripcion}</p>
+                        <a href="./pages/movieTh1.html" id="${el.nommbre}">Buy Ticket</a>
+                    </div>
+            </div>
+            `
+            cardClassicCreator.appendChild(div)
+            const backGroundToLs = document.getElementById(`${el.nommbre}`)
+            backGroundToLs.addEventListener("click", () => {
+                console.log(`${el.nommbre}`)
+                localStorage.setItem("backGroundOnLs", `${el.nommbre}`);
+            })
+            VanillaTilt.init(document.querySelectorAll(".card"), {
+                max: 4,
+                speed: 400
+                });
+        })
+    }
+    
+    movieCardClassic();
+})
+
+
+
+
+
+
+
+// const moviesCassic = [
+//     {
+//         nommbre: "pulpFiction",
+//         duracion: 120,
+//         rate: "ATP",
+//         descripcion: "Es una re pelicula re copada con muchos efectos",
+//         sala: "04",
+//         movieId: 001,
+//         img:"imgThirteen", 
+//     },
+//     {
+//         nommbre: "taxiDriver",
+//         duracion: 120,
+//         rate: "ATP",
+//         descripcion: "Es una re pelicula re copada con muchos efectos",
+//         sala: "05",
+//         movieId: 001,
+//         img:"imgFourteen", 
+//     },
+//     {
+//         nommbre: "godFather",
+//         duracion: 120,
+//         rate: "ATP",
+//         descripcion: "Es una re pelicula re copada con muchos efectos",
+//         sala: "06",
+//         movieId: 001,
+//         img:"imgFifteen", 
+//     },
+// ]
 
 const cardPremmiereCreator = document.querySelector('#movie-cards-premmiere');
 const cardRecommendedCreator = document.querySelector('#movie-cards-recommended');
 const cardClassicCreator = document.querySelector('#movie-cards-classic');
 
-//cards Premmiere
 
-// function movieCardPremmiere(){
-//     moviesPremiere.forEach(el => {
+
+//Card Classic
+
+// function movieCardClassic(){
+//     moviesCassic.forEach(el => {
 //         const div = document.createElement('div');
 //         div.innerHTML = `
 //         <div class="card ${el.img}" id="card-1">
@@ -118,11 +166,11 @@ const cardClassicCreator = document.querySelector('#movie-cards-classic');
 //                     <h2>${el.sala}</h2>
 //                     <h3>Movie theatre</h3>
 //                     <p>${el.descripcion}</p>
-//                     <a href="./pages/movieTh1.html" id="${el.nommbre}">Buy Ticket</a>
+//                     <a class="btn-buy" href="./pages/movieTh1.html" id="${el.nommbre}">Buy Ticket</a>
 //                 </div>
 //         </div>
 //         `
-//         cardPremmiereCreator.appendChild(div)
+//         cardClassicCreator.appendChild(div)
 //         const backGroundToLs = document.getElementById(`${el.nommbre}`)
 //         backGroundToLs.addEventListener("click", () => {
 //             console.log(`${el.nommbre}`)
@@ -132,69 +180,7 @@ const cardClassicCreator = document.querySelector('#movie-cards-classic');
 //             max: 4,
 //             speed: 400
 //             });
-            
 //     })
 // }
 
-// movieCardPremmiere();
-
-
-//Cards Recommended
-
-function movieCardRecommended(){
-    moviesRecommended.forEach(el => {
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <div class="card ${el.img}" id="card-1">
-                <div class="content">
-                    <h2>${el.sala}</h2>
-                    <h3>Movie theatre</h3>
-                    <p>${el.descripcion}</p>
-                    <a href="./pages/movieTh1.html" id="${el.nommbre}">Buy Ticket</a>
-                </div>
-        </div>
-        `
-        cardRecommendedCreator.appendChild(div)
-        const backGroundToLs = document.getElementById(`${el.nommbre}`)
-        backGroundToLs.addEventListener("click", () => {
-            console.log(`${el.nommbre}`)
-            localStorage.setItem("backGroundOnLs", `${el.nommbre}`);
-        })
-        VanillaTilt.init(document.querySelectorAll(".card"), {
-            max: 4,
-            speed: 400
-            });
-    })
-}
-
-movieCardRecommended();
-
-//Card Classic
-
-function movieCardClassic(){
-    moviesCassic.forEach(el => {
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <div class="card ${el.img}" id="card-1">
-                <div class="content">
-                    <h2>${el.sala}</h2>
-                    <h3>Movie theatre</h3>
-                    <p>${el.descripcion}</p>
-                    <a class="btn-buy" href="./pages/movieTh1.html" id="${el.nommbre}">Buy Ticket</a>
-                </div>
-        </div>
-        `
-        cardClassicCreator.appendChild(div)
-        const backGroundToLs = document.getElementById(`${el.nommbre}`)
-        backGroundToLs.addEventListener("click", () => {
-            console.log(`${el.nommbre}`)
-            localStorage.setItem("backGroundOnLs", `${el.nommbre}`);
-        })
-        VanillaTilt.init(document.querySelectorAll(".card"), {
-            max: 4,
-            speed: 400
-            });
-    })
-}
-
-movieCardClassic();
+// movieCardClassic();
